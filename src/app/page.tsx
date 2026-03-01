a86505ab9798d6c19241e81110c678fdc8c387d6
agadjuka <161559115+agadjuka@users.noreply.github.com>
2026-03-01 14:45:47 +0800

    Мобильное

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,19 +15,21 @@ import { PreloadVirtualTryOnImages } from '@/components/home/virtual-tryon/Prelo
 
 export default function Home() {
   return (
-    <div className="relative flex min-h-screen w-full flex-col grid-lines font-body antialiased overflow-x-hidden selection:bg-amber-500 selection:text-black">
+    <div className="fixed inset-0 flex flex-col h-[100dvh] w-full overflow-hidden overflow-x-hidden grid-lines font-body antialiased selection:bg-amber-500 selection:text-black lg:static lg:min-h-screen lg:h-auto">
       <PreloadVirtualTryOnImages />
       <BackgroundScanLines />
       <HomeHeader />
-      <main className="flex-1 flex flex-col relative">
-        <HomeHeroSection />
-        <ManifestoSection />
-        <CoreTechnologiesHeading />
-        <AutonomousIntelligenceSection />
-        <DigitalAtelierSection />
-        <BespokeEngineeringSection />
-        <HomeFooter />
-        <CommandBar />
+      <main className="flex-1 flex flex-col relative min-h-0 overflow-y-auto overflow-x-hidden overscroll-contain touch-pan-y scrollable-content lg:overflow-visible lg:min-h-0">
+        <div className="shrink-0 flex flex-col">
+          <HomeHeroSection />
+          <ManifestoSection />
+          <CoreTechnologiesHeading />
+          <AutonomousIntelligenceSection />
+          <DigitalAtelierSection />
+          <BespokeEngineeringSection />
+          <HomeFooter />
+          <CommandBar />
+        </div>
       </main>
       <ScrollRevealObserver />
     </div>
